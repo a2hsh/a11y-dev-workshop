86d5944357d7560a3c2106bb4fea5a5c850b2149
Abdulaziz Alshmasi <a2h.alshmasi@gmail.com>
2025-07-21 19:30:00 +0300

    Setup Base Name in react-router.config.ts

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,7 +1,10 @@
 import type { Config } from "@react-router/dev/config";
 
+
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: false,
+  // Explicitly set basename for GitHub Pages
+  basename: process.env.NODE_ENV === "production" ? "/a11y-dev-workshop/" : "/",
 } satisfies Config;
